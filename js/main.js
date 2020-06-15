67eb818065813573b0977803c5c3499ae2a55e22
iwahid-resume <57856819+iwahid-resume@users.noreply.github.com>
2020-06-15 18:58:47 +0500

    Feat (header): added a search field in to Header of site
    Add search box (search field with a search button and category selector)

---
+++ js/main.js
@@ -0,0 +1,19 @@
+console.log("fired!")
+
+const selectElement = document.querySelector("[data-select]")
+const selectReal = document.getElementById("selectReal")
+
+selectElement.addEventListener('click', function (event) {
+
+  if (event.target.hasAttribute("data-select-item")) {
+    const itemTitle = event.target.getAttribute("data-select-item") //mobile /* или */ /* dataset["select-item"] */
+    console.log(itemTitle)
+    event.target.closest("[data-select]").querySelector("[data-select-title]").innerText = itemTitle // нахожу родителя, что бы через него выйти на title
+    event.target.closest("[data-select]").querySelector(".header-select__dropdown").classList.toggle("hidden") //после выбора параметра, скрываю всё обратно
+
+    selectReal.value = itemTitle
+    console.log("Значение selectReal: ", selectReal.value)
+  } else {
+    this.querySelector(".header-select__dropdown").classList.toggle("hidden")
+  }
+})
