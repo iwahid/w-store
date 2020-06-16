947ba9683b8214487bd50f868eeb443f4f5c6f22
iwahid-resume <57856819+iwahid-resume@users.noreply.github.com>
2020-06-16 15:59:53 +0500

    Refactor (header): added comment to js script

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@ console.log("fired!")
 const selectElement = document.querySelector("[data-select]")
 const selectReal = document.getElementById("selectReal")
 
+/* чекаю на уровне всего селекта-контейнера */
 selectElement.addEventListener('click', function (event) {
 
   if (event.target.hasAttribute("data-select-item")) {
     const itemTitle = event.target.getAttribute("data-select-item") //mobile /* или */ /* dataset["select-item"] */
     console.log(itemTitle)
-    event.target.closest("[data-select]").querySelector("[data-select-title]").innerText = itemTitle // нахожу родителя, что бы через него выйти на title
+    event.target.closest("[data-select]").querySelector("[data-select-title]").innerText = itemTitle // нахожу родителя, что бы через него выйти на title и закинуть в него значение
     event.target.closest("[data-select]").querySelector(".header-select__dropdown").classList.toggle("hidden") //после выбора параметра, скрываю всё обратно
 
     selectReal.value = itemTitle
@@ -16,4 +17,5 @@ selectElement.addEventListener('click', function (event) {
   } else {
     this.querySelector(".header-select__dropdown").classList.toggle("hidden")
   }
+
 })
