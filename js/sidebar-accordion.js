8898deba91b9741dc80477b12de8c4bc17136f32
iwahid-resume <57856819+iwahid-resume@users.noreply.github.com>
2020-07-13 19:11:00 +0500

    Fix (footer): separate blocks

--- js/sidebar-accordion.js
+++ js/sidebar-accordion.js
@@ -1,10 +1,17 @@
-var acc = document.getElementsByClassName("navigation-item");
+var acc = document.getElementsByClassName("main-navigation__item");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    this.classList.toggle("navigation-item_selected");
+    this.classList.toggle("main-navigation__item_selected");
     var panel = this.nextElementSibling;
+
+    /* анимирование поворота стрелочки */
+  /*   if (this.childNodes[7].childNodes[1].classList.contains("main-navigation__item-arrow-icon"))
+        console.log(123)
+        console.log(this.childNodes[7].childNodes[1]) */
+    this.childNodes[7].childNodes[1].classList.toggle("adapt")
+
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
